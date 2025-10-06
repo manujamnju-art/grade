2f5c48e75ea1a8358c13d1d835585d02c43d389a
manujamnju-art <manujamnju@gmail.com>
2025-10-06 13:57:27 +0530

    save main

---
+++ grade.py
@@ -0,0 +1,13 @@
+marks = input("enter the marks")
+if marks >= 90:
+    print("Grade : A")
+elif marks >= 80:
+    print("Grade:B")
+elif marks >= 70:
+    print("Grade:c")
+elif marks >= 60:
+    print("grade:D")
+elif marks >= 60:
+    print("Grade:E")
+elif marks >= 50:
+    print("Grade:F")
